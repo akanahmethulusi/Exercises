b8d7ed0ade21f4e87f814caeb85265b47a6058c4
Ahmet Hulusi Akan <95524111+akanahmethulusi@users.noreply.github.com>
2024-05-14 11:59:05 +0200

    Update StringLastIndexOf.java

--- src/StringLastIndexOf.java
+++ src/StringLastIndexOf.java
@@ -1,7 +1,7 @@
 public class StringLastIndexOf {
     public static void main(String[] args){
         String str;
-        str = "on besinci aramayı duydum mucuk ";
+        str = "fünfzehnte anruf habe ich nicht gehört ";
         str = str.trim();
         int lastIndex = str.lastIndexOf(" ")+1;
         //System.out.println(str.substring(lastIndex, str.length()));
