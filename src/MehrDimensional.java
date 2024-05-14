ad26ab5e51730f91abb07fa346e715c451087d71
Ahmet Hulusi Akan <95524111+akanahmethulusi@users.noreply.github.com>
2024-05-14 11:55:51 +0200

    Update MehrDimensional.java

--- src/MehrDimensional.java
+++ src/MehrDimensional.java
@@ -8,12 +8,12 @@ class MehrDimensionalSub{
     public static void mehrDimensionalSub(){
         String[][] firstAndLastName = new String[5][2];
 
-        firstAndLastName[0][0] = "ahmet";
-        firstAndLastName[0][1] = "akan";
-        firstAndLastName[1][0] = "muaz";
-        firstAndLastName[1][1] = "akan";
-        firstAndLastName[2][0] = "mehlika";
-        firstAndLastName[2][1] = "akan";
+        firstAndLastName[0][0] = "Hannes";
+        firstAndLastName[0][1] = "Klemen";
+        firstAndLastName[1][0] = "Otto";
+        firstAndLastName[1][1] = "Bukelemen";
+        firstAndLastName[2][0] = "Henrich";
+        firstAndLastName[2][1] = "Stamm";
 
         for(int i=0; i < firstAndLastName.length; i++){
             for(int j=0; j<firstAndLastName[i].length; j++) {
