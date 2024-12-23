47ad6b8247eeae2958d921f4a27b454f507beaf7
Ahmet Hulusi Akan <akanahmethulusi@gmail.com>
2024-12-23 17:16:20 +0100

    added new exercise class RomanToInteger.java

---
+++ src/RomanToInteger.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class RomanToInteger {
+    public static void main(String[] args) {
+        romanToInteger("LXVII");
+    }
+
+    public static int romanToInteger(String s){
+        Map<Character, Integer> m = new HashMap<>();
+        m.put('I',1);
+        m.put('V',5);
+        m.put('X',10);
+        m.put('L',50);
+        m.put('C',100);
+        m.put('D',500);
+        m.put('M',1000);
+
+        int ans = 0;
+
+        for (int i=0; i< s.length(); i++){
+            if(i< s.length()-1  && m.get(s.charAt(i)) < m.get(s.charAt(i+1))){
+                ans = ans - m.get(s.charAt(i));
+            }else {
+                ans = ans + m.get(s.charAt(i));
+            }
+        }
+
+        return ans;
+    }
+}
